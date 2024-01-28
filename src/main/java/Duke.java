3ae603a5032eb2701208dc022767e55d926ed678
Rouvin <rouvinerh@u.nus.edu>
2024-01-28 11:08:42 +0800

    Add Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+    public static String horizontalLine = "__________________________________________________\n";
+
+    public static void startUp() {
+        String greeting = horizontalLine
+                + "Hello! I'm ByteBrew!\n"
+                + "What can I do for you?\n"
+                + horizontalLine;
+        System.out.print(greeting);
+    }
+
+    public static void shutDown() {
+        String farewell = horizontalLine
+                + "Bye. Hope to see you again soon!\n"
+                + horizontalLine;
+        System.out.print(farewell);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        startUp();
+        shutDown();
     }
 }
