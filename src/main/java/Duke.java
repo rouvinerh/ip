c02ce69506c7d3c3637ee142f65ccb7616911024
Rouvin <rouvinerh@u.nus.edu>
2024-01-28 11:21:36 +0800

    Add Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,31 +10,49 @@ public class Duke {
         System.out.print(greeting);
     }
 
-    public static void echoInput(String line) {
-        String output = horizontalLine
-                + line
-                + "\n"
-                + horizontalLine;
-        System.out.print(output);
-    }
-
     public static void shutDown() {
         String farewell = horizontalLine
                 + "Bye. Hope to see you again soon!\n"
                 + horizontalLine;
         System.out.print(farewell);
     }
+
+    public static void printTasks(String[] taskArray, int taskCounter) {
+        System.out.print(horizontalLine);
+        System.out.println("Here's the task list: ");
+
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println((i + 1) + ". " + taskArray[i]);
+        }
+        System.out.print(horizontalLine);
+    }
     public static void main(String[] args) {
         startUp();
 
         Scanner in = new Scanner(System.in);
         String inputLine = in.nextLine();
+        String[] taskArray = new String[100];
+        int taskCounter = 0;
 
-        while (!inputLine.equals("bye")) {
-            echoInput(inputLine);
+        while (true) {
+            if (inputLine.equals("bye")) {
+                shutDown();
+                return;
+            }
+
+            else if (inputLine.equals("list")) {
+                printTasks(taskArray, taskCounter);
+            }
+
+            else {
+                taskArray[taskCounter] = inputLine;
+                taskCounter += 1;
+                System.out.print(horizontalLine);
+                System.out.println("Added: " + inputLine);
+                System.out.println("Number of Tasks: " + taskCounter);
+                System.out.print(horizontalLine);
+            }
             inputLine = in.nextLine();
         }
-        
-        shutDown();
     }
 }
