69de547b3f148480a268adeae5558249241a1946
Rouvin <rouvinerh@gmail.com>
2024-02-12 22:57:56 +0800

    Revert "Merge branch 'branch-level-5'"
    This reverts commit 9ab8527c6a06c8c81785c8bd856a0c716d37e808, reversing
    changes made to fc0c1e21c08f62df06ad031e794f657dce6ff173.

--- src/main/java/ByteBrewException.java
+++
@@ -1,6 +0,0 @@
-public class ByteBrewException extends Exception {
-    public static String HORIZONTAL_LINE = "__________________________________________________\n";
-    public ByteBrewException(String message) {
-        super(HORIZONTAL_LINE + message + "\n" + HORIZONTAL_LINE);
-    }
-}
