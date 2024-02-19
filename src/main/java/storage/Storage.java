547e2b311ae901dca12bb80964220c2724bcb18d
Rouvin <rouvinerh@gmail.com>
2024-02-19 12:18:39 +0800

    Abstract storage method

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -28,62 +28,24 @@ public class Storage {
         try {
             File dataFile = new File("bytebrew_data.txt");
 
-            if (dataFile.createNewFile()) {
-                System.out.println(constants.HORIZONTAL_LINE);
-                System.out.println("Data file NOT present. Creating one now!");
-                System.out.println(constants.HORIZONTAL_LINE);
-            } else {
-                System.out.println(constants.HORIZONTAL_LINE);
-                System.out.println("Data file present!");
-                System.out.println(constants.HORIZONTAL_LINE);
-            }
+            checkDataFile(dataFile);
 
             final Scanner READ_FILE = new Scanner(dataFile);
             while (READ_FILE.hasNext()) {
                 String[] task = READ_FILE.nextLine().split("\\|");
                 switch (task[0].trim()) {
                 case "todo": {
-                    String toDoDescription = task[2];
-                    Todo newTodo = new Todo(toDoDescription);
-                    tasks.add(newTodo);
-                    checkIsDone(tasks, task[1].trim(), taskCount);
+                    processToDo(tasks, task, taskCount);
                     break;
                 }
 
                 case "deadline": {
-                    String[] deadlineInfo = task[2].split("by:");
-                    String by;
-                    if (deadlineInfo.length > 1) {
-                        by = deadlineInfo[1].trim().substring(0, deadlineInfo[1].length() - 1);
-                    }
-                    else {
-                        throw new ByteBrewException("Invalid deadline format!");
-                    }
-                    Deadline deadline = new Deadline(deadlineInfo[0].trim(), by);
-                    tasks.add(deadline);
-                    checkIsDone(tasks, task[1].trim(), taskCount);
+                    processDeadline(tasks, task, taskCount);
                     break;
                 }
 
                 case "event": {
-                    String[] eventInfo = task[2].split("from:");
-
-                    if (eventInfo.length < 2) {
-                        throw new ByteBrewException("Invalid event format");
-                    }
-                    String eventDescription = eventInfo[0].trim();
-
-                    String[] eventTimes = eventInfo[1].split(" to:");
-                    if (eventTimes.length < 2) {
-                        throw new ByteBrewException("Invalid event format");
-                    }
-
-                    String from = eventTimes[0].trim();
-                    String to = eventTimes[1].trim();
-
-                    Event event = new Event(eventDescription, from, to);
-                    tasks.add(event);
-                    checkIsDone(tasks, task[1].trim(), taskCount);
+                    processEvent(tasks, task, taskCount);
                     break;
                 }
                 default: {
@@ -100,7 +62,61 @@ public class Storage {
         }
     }
 
-    public static void writeFile (ArrayList<Task> tasks) throws ByteBrewException {
+    public static void processEvent(ArrayList<Task> tasks, String[] task, int taskCount) throws ByteBrewException {
+        String[] eventInfo = task[2].split("from:");
+
+        if (eventInfo.length < 2) {
+            throw new ByteBrewException("Invalid event format");
+        }
+        String eventDescription = eventInfo[0].trim();
+
+        String[] eventTimes = eventInfo[1].split(" to:");
+        if (eventTimes.length < 2) {
+            throw new ByteBrewException("Invalid event format");
+        }
+
+        String from = eventTimes[0].trim();
+        String to = eventTimes[1].trim();
+
+        Event event = new Event(eventDescription, from, to);
+        tasks.add(event);
+        checkIsDone(tasks, task[1].trim(), taskCount);
+    }
+
+    public static void processDeadline(ArrayList<Task> tasks, String[] task, int taskCount) throws ByteBrewException {
+        String[] deadlineInfo = task[2].split("by:");
+        String by;
+        if (deadlineInfo.length > 1) {
+            by = deadlineInfo[1].trim().substring(0, deadlineInfo[1].length() - 1);
+        }
+        else {
+            throw new ByteBrewException("Invalid deadline format!");
+        }
+        Deadline deadline = new Deadline(deadlineInfo[0].trim(), by);
+        tasks.add(deadline);
+        checkIsDone(tasks, task[1].trim(), taskCount);
+    }
+
+    public static void processToDo(ArrayList<Task> tasks, String[] task, int taskCount) throws ByteBrewException {
+        String toDoDescription = task[2];
+        Todo newTodo = new Todo(toDoDescription);
+        tasks.add(newTodo);
+        checkIsDone(tasks, task[1].trim(), taskCount);
+    }
+
+    public static void checkDataFile(File dataFile) throws IOException {
+        if (dataFile.createNewFile()) {
+            System.out.println(constants.HORIZONTAL_LINE);
+            System.out.println("Data file NOT present. Creating one now!");
+            System.out.println(constants.HORIZONTAL_LINE);
+        } else {
+            System.out.println(constants.HORIZONTAL_LINE);
+            System.out.println("Data file present!");
+            System.out.println(constants.HORIZONTAL_LINE);
+        }
+    }
+
+    public static void writeFile(ArrayList<Task> tasks) throws ByteBrewException {
         try {
             FileWriter dataFile = new FileWriter("bytebrew_data.txt");
 
